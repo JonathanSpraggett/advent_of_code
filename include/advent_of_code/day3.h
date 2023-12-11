0b69bed44326b007b52b7af42b13b69de6aa7dd1
manx52 <jonathanspraggett@gmail.com>
2023-12-11 12:19:27 -0500

    f

---
+++ include/advent_of_code/day3.h
@@ -0,0 +1,16 @@
+//
+// Created by jonathan on 11/12/23.
+//
+
+#ifndef DAY3_H
+#define DAY3_H
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int day3();
+#endif //DAY3_H
---
+++ src/day3.cpp
@@ -0,0 +1,9 @@
+//
+// Created by jonathan on 11/12/23.
+//
+
+#include <advent_of_code/day3.h>
+
+int day3() {
+    return 0;
+}
